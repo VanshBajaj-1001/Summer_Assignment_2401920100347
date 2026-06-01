efdb5d868d4c6b1332117db05fd0a0d4cd0e6564
Vansh Bajaj <vanshbajaj1001@gmail.com>
2026-06-01 17:03:41 +0530

    Added Remove Duplicate From Sorted Array

---
+++ Week_1/Day_1/Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+    int i,j;
+    i=0;
+    for(j=1;j<nums.size();j++)
+    {
+        if(nums[i]!=nums[j])
+        {
+            i++;
+            nums[i]=nums[j];
+        }
+    }
+    return i+1;;
+    }
+};
