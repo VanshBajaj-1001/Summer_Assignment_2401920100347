22261ed3fa5668fe403afd7da9a31498f3a6a505
Vansh Bajaj <vanshbajaj1001@gmail.com>
2026-06-02 17:40:01 +0530

    Added day 3 2nd soln

---
+++ Week_1/Day_3/Square_of_Sorted_Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]>nums[j]){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        return nums;
+    }
+};
